4e40ceae47df6fed27b241c56e70da24f3bdedfa
Ahoumenou Emile <ahoumenouemile22@gmail.com>
2023-06-09 18:35:34 +0100

    first commit

---
+++ instructions/Rendu/index.js
@@ -0,0 +1,12 @@
+// Page Home
+
+
+ 
+$(document).ready(function(){
+    $((".parent")).slick({
+       slidesToShow: 1,
+       dots: true,
+       autoplay: true,
+       autoplaySpeed: 5000
+    })
+})
